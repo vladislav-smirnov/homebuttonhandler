2cdcf07650c065b579a1c5e925c4fba1d70ac31c
Vladislav Smirnov <sivdead@gmail.com>
2020-09-19 18:32:10 +0300

    [Project] Init commit.
    Uploaded basic code.

---
+++ app/src/main/java/io/github/airdaydreamers/homebuttonhandler/HomeActivity.kt
@@ -0,0 +1,11 @@
+package io.github.airdaydreamers.homebuttonhandler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
